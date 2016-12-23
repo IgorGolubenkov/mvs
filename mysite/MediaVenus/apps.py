9e452a7b8dbe48bcfde82aa43587568b5e8b0bed
DESKTOP-K4L8N48\Игорь Тест <iogolubenkov@8bitov.com>
2016-12-23 14:24:18 +0300

    add new django project

---
+++ mysite/MediaVenus/apps.py
@@ -0,0 +1,5 @@
+
+from django.apps import AppConfig
+
+class MediaVenusConfig(AppConfig):
+    name = 'MediaVenus'
---
+++ mysite/MediaVenus/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django import forms
+
+
+# class MediavenusWidget(forms.Form):
+#     class Media:
+#         js = ('mediavenus.js')
+
+class MediaVenus(models.Model):
+    tizer = models.Field()
